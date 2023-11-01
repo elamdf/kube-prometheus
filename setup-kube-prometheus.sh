b88a3e30cc7b36c657af72932e0301b2738776cc
elam <elamdf@berkeley.edu>
2023-11-01 00:32:51 +0000

    tidying

--- setup-kube-prometheus.sh
+++ setup-kube-prometheus.sh
@@ -1,4 +1,5 @@
-./node_exporter &
+#./node_exporter &
+#gnmic --config gnmic-config.yml  --log subscribe & 
 kubectl delete --ignore-not-found=true -f manifests/ -f manifests/setup
 set -e
 kubectl apply --server-side -f manifests/setup
@@ -7,5 +8,7 @@ kubectl wait \
 	--all CustomResourceDefinition \
 	--namespace=monitoring
 kubectl apply -f manifests/
-sleep 25
+kubectl apply -n istio-system -f ~/istio-1.15.0/samples/addons/extras/prometheus-operator.yaml
+sleep 10
+kubectl wait --for=condition=ready --namespace monitoring pods/prometheus-k8s-0
 kubectl --namespace monitoring port-forward svc/prometheus-k8s 9090
